c07cc20339aaf5eed7ad637d22534c12f3a31bec
Daniel Feitosa <danielfeitosa.gama@outlook.com>
2023-04-25 14:04:31 -0300

    aplicando modal no banner

---
+++ js/script.js
@@ -0,0 +1,22 @@
+// /* MODAL MURO DE DAVI */
+// const botaoMuroDeDaviOpen = document.querySelector('#btn-muro-davi')
+// const botaoMuroDeDaviClose = document.querySelector('#btn-close-muro-davi')
+// const muroDeDaviModal = document.querySelector('#modalPrincipal')
+// const corpoDaPagina = document.querySelector('body')
+
+// const abrirModal = (modal) => {
+//   modal.style.display = 'block'
+// }
+
+// const fecharModal = (modal) => {
+//   modal.style.display = 'none'
+// }
+
+// botaoMuroDeDaviOpen.addEventListener('click', () => {
+//   abrirModal(muroDeDaviModal)
+
+// })
+
+// botaoMuroDeDaviClose.addEventListener('click', () => {
+//   fecharModal(muroDeDaviModal)
+// })
